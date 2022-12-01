1cb6f38ffdc57c00352587e5e395aa7cd7f82d2d
leximainw <leximainw@gmail.com>
2022-12-01 08:53:23 -0500

    feat: create framework for selecting problem solver

---
+++ src/day1.rs
@@ -0,0 +1,6 @@
+use std::fs;
+
+pub fn day1a() {
+    let text = fs::read_to_string("inputs/day1a.txt").unwrap();
+    println!("{text}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod day1;
+
+use std::io::{
+    BufRead,
+    stdin,
+    stdout,
+    Write,
+};
+
+use day1::day1a;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("Select day: ");
+        stdout().flush().unwrap();
+        let value = stdin().lock().lines().next().unwrap().unwrap();
+        match value.as_str() {
+            "1a" => day1a(),
+            "exit" => break,
+            _ => println!("Unknown option {value}"),
+        }
+    }
 }
