1cea717116b32ac477dbf9119f49b74217a40a40
leximainw <leximainw@gmail.com>
2022-12-11 16:50:00 -0500

    refactor: move `day1` function to the top of `day1.rs`

--- src/solvers/day1.rs
+++ src/solvers/day1.rs
@@ -2,6 +2,21 @@ use std::error::Error;
 use std::fs;
 use std::str::Lines;
 
+pub fn day1(best_n: usize) -> Result<(), Box<dyn Error>> {
+    let elves = read_elves(fs::read_to_string("inputs/day1.txt")?.lines())?;
+    let mut sum = 0;
+    for i in 0..best_n {
+        sum += elves[i].1
+    }
+    println!(
+        "The top {} carrying {} calories.",
+        if best_n == 1 { format!("elf is") }
+        else { format!("{} elves are", best_n) },
+        sum,
+    );
+    Ok(())
+}
+
 fn read_elves(lines: Lines) -> Result<Vec<(i32, i32)>, Box<dyn Error>> {
     let mut elves = Vec::new();
     let mut sum = 0;
@@ -17,19 +32,4 @@ fn read_elves(lines: Lines) -> Result<Vec<(i32, i32)>, Box<dyn Error>> {
     }
     elves.sort_by(|l, r| r.1.partial_cmp(&l.1).unwrap());
     Ok(elves)
-}
-
-pub fn day1(best_n: usize) -> Result<(), Box<dyn Error>> {
-    let elves = read_elves(fs::read_to_string("inputs/day1.txt")?.lines())?;
-    let mut sum = 0;
-    for i in 0..best_n {
-        sum += elves[i].1
-    }
-    println!(
-        "The top {} carrying {} calories.",
-        if best_n == 1 { format!("elf is") }
-        else { format!("{} elves are", best_n) },
-        sum,
-    );
-    Ok(())
 }
