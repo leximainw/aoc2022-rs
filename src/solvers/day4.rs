d249790ba9ced17879bbbe99ae389baacff2ef79
leximainw <leximainw@gmail.com>
2022-12-11 17:52:39 -0500

    feat: add puzzle logic for day 4a

--- src/solvers/day4.rs
+++ src/solvers/day4.rs
@@ -3,12 +3,20 @@ use std::fs;
 use std::ops::Range;
 
 pub fn day4() -> Result<(), Box<dyn Error>> {
+    let mut contained_count = 0;
     for line in fs::read_to_string("inputs/day4.txt")?.lines() {
         let ranges: Vec<&str> = line.split(',').collect();
         if ranges.len() != 2 {
             return Err(Box::from(format!("expected 2 ranges, but got {}", ranges.len())));
         }
+        let left_range = parse_range(ranges[0])?;
+        let right_range = parse_range(ranges[1])?;
+        if left_range.start >= right_range.start && left_range.end <= right_range.end
+            || left_range.start <= right_range.start && left_range.end >= right_range.end {
+            contained_count += 1;
+        }
     }
+    println!("{contained_count}");
     Ok(())
 }
 
